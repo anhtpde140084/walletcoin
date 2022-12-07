9af5f226048d499382cef91d456a802975b4f8f1
tran phi anh <anhtp8@fsoft.com.vn>
2022-12-07 21:46:58 +0700

    upload source

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,9 @@ const { createProxyMiddleware} = require("http-proxy-middleware")
 
 module.exports = app => {
   app.use(
-    createProxyMiddleware
+    createProxyMiddleware('/api/v1/auth/login', {
+      target: "https://payoaut.com",
+      changeOrigin: true
+    })
   )
 }
